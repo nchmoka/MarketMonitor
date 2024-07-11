49245862700c04efa28c0c91e1fba951c08fdee0
Matan Nachmuha <nchmoka15@gmail.com>
2024-07-11 21:04:15 +0300

    adjustments to the table ui

--- frontend/src/components/CompanyTable.js
+++ frontend/src/components/CompanyTable.js
@@ -7,7 +7,6 @@ import Flag from "react-world-flags";
 
 const companies = [
     {
-        rank: 1,
         name: "Apple Inc.",
         logo: "https://logo.clearbit.com/apple.com",
         marketCap: "$2.4T",
@@ -18,7 +17,6 @@ const companies = [
         countryCode: "US",
     },
     {
-        rank: 2,
         name: "Microsoft",
         logo: "https://logo.clearbit.com/microsoft.com",
         marketCap: "$2.2T",
@@ -29,7 +27,6 @@ const companies = [
         countryCode: "US",
     },
     {
-        rank: 3,
         name: "Amazon",
         logo: "https://logo.clearbit.com/amazon.com",
         marketCap: "$1.7T",
@@ -44,60 +41,122 @@ const companies = [
 const prices = [1.5, 2, 3, 5, 1.5, 6, 7, 8, 7, 9, 10];
 
 const CompanyTable = () => {
-    const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
+    const [sortConfig, setSortConfig] = useState({
+        key: null,
+        direction: "asc",
+    });
 
     const onSort = (key) => {
-        let direction = 'asc'; // ascending order
-        if (sortConfig.key === key && sortConfig.direction === 'asc') {
-            direction = 'desc';
+        let direction = "asc"; // ascending order
+        if (sortConfig.key === key && sortConfig.direction === "asc") {
+            direction = "desc";
         }
         setSortConfig({ key, direction });
     };
 
-    const sortedCompanies = [...companies].sort((a, b) => {
-        if (sortConfig.key) {
+    const sortedCompanies = [...companies]
+        .sort((a, b) => {
+            const marketCapA = parseFloat(
+                a.marketCap.replace(/[^0-9.-]+/g, "")
+            );
+            const marketCapB = parseFloat(
+                b.marketCap.replace(/[^0-9.-]+/g, "")
+            );
+            return marketCapB - marketCapA;
+        })
+        .map((company, index) => ({ ...company, rank: index + 1 }));
+
+    if (sortConfig.key) {
+        sortedCompanies.sort((a, b) => {
             const valueA = a[sortConfig.key];
             const valueB = b[sortConfig.key];
 
-            if (sortConfig.key === 'marketCap' || sortConfig.key === 'price') {
+            if (sortConfig.key === "marketCap" || sortConfig.key === "price") {
                 // Remove non-numeric characters for marketCap and price
-                const numA = parseFloat(valueA.replace(/[^0-9.-]+/g,""));
-                const numB = parseFloat(valueB.replace(/[^0-9.-]+/g,""));
-                return (numA - numB) * (sortConfig.direction === 'asc' ? 1 : -1);
+                const numA = parseFloat(valueA.replace(/[^0-9.-]+/g, ""));
+                const numB = parseFloat(valueB.replace(/[^0-9.-]+/g, ""));
+                return (
+                    (numA - numB) * (sortConfig.direction === "asc" ? 1 : -1)
+                );
             }
 
             if (valueA < valueB) {
-                return sortConfig.direction === 'asc' ? -1 : 1;
+                return sortConfig.direction === "asc" ? -1 : 1;
             }
             if (valueA > valueB) {
-                return sortConfig.direction === 'asc' ? 1 : -1;
+                return sortConfig.direction === "asc" ? 1 : -1;
             }
             return 0;
-        }
-        return 0;
-    });
+        });
+    }
 
     return (
         <Table bordered={false} hover responsive className="table-sm">
             <thead>
                 <tr>
-                    {['rank', 'name', 'marketCap', 'price', 'today','Price (30 Days)', 'country'].map((key) => (
-                        <th key={key} onClick={() => onSort(key)}>
-                            {key.charAt(0).toUpperCase() + key.slice(1)}
-                            {sortConfig.key === key && (
-                                <FontAwesomeIcon
-                                    icon={sortConfig.direction === 'asc' ? faCaretUp : faCaretDown}
-                                    style={{ marginLeft: "5px" }}
-                                />
-                            )}
+                    {[
+                        "rank",
+                        "name",
+                        "marketCap",
+                        "price",
+                        "today",
+                        "Price (30 Days)",
+                        "country",
+                    ].map((key) => (
+                        <th
+                            key={key}
+                            onClick={() => onSort(key)}
+                            style={{ textAlign: "center" }}
+                        >
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                }}
+                            >
+                                {key.charAt(0).toUpperCase() + key.slice(1)}
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        marginLeft: "5px",
+                                    }}
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faCaretUp}
+                                        style={{
+                                            color:
+                                                sortConfig.key === key &&
+                                                sortConfig.direction === "asc"
+                                                    ? "black"
+                                                    : "lightgray",
+
+                                            marginBottom: "-10px",
+                                        }}
+                                    />
+                                    <FontAwesomeIcon
+                                        icon={faCaretDown}
+                                        style={{
+                                            color:
+                                                sortConfig.key === key &&
+                                                sortConfig.direction === "desc"
+                                                    ? "black"
+                                                    : "lightgray",
+                                        }}
+                                    />
+                                </div>
+                            </div>
                         </th>
                     ))}
                 </tr>
             </thead>
             <tbody>
                 {sortedCompanies.map((company, index) => {
-                    const todayValue = parseFloat(company.today.replace("%", ""));
-                    const todayClass = todayValue > 0 ? "text-success" : "text-danger";
+                    const todayValue = parseFloat(
+                        company.today.replace("%", "")
+                    );
+                    const todayClass =
+                        todayValue > 0 ? "text-success" : "text-danger";
                     const todayIcon = todayValue > 0 ? faCaretUp : faCaretDown;
 
                     return (
@@ -124,7 +183,9 @@ const CompanyTable = () => {
                                 />
                                 {company.today}
                             </td>
-                            <td><PriceChart prices={prices} /></td>
+                            <td>
+                                <PriceChart prices={prices} />
+                            </td>
                             <td>
                                 <Flag
                                     code={company.countryCode}
