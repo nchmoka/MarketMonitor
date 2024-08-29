761734f1ce3250fc2d0cc3c87dd1dd51e878c93e
ndvp39 <ndvp39@gmail.com>
2024-08-29 16:13:44 +0300

    vercel upload

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,9 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
